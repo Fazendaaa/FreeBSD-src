b0e3db4f25c47c19c0f892ced7180f311576b948
Fazendaaa <lucas.carotta@outlook.com>
2020-08-29 11:46:15 -0300

    adding libre script

---
+++ libre.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git checkout RELENG_2_4_5
+# https://stackoverflow.com/a/48280659/7092954
+find ./release/conf/ -type f | perl -pe 'print $_; s/pfSense/libreSense/' | xargs -n2 sudo mv
+sudo mv ./sys/amd64/conf/pfSense ./sys/amd64/conf/libreSense
+# https://askubuntu.com/a/879323
+perl -pe 's/cryptokeytest//g' ./tools/tools/crypto/Makefile
